f051a0a15179ea97d456f0e64032952405f41372
Meetali Kapse <102135323+meetalik8@users.noreply.github.com>
2023-10-29 21:10:17 +0530

    S3 lambda function trigger for image upload

--- advdevops/lambdaimagetrigger.py
+++ advdevops/lambdaimagetrigger.py
@@ -6,3 +6,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('An Image has been added!')
     }
+
+# Create a S3 bucket (unblock public access) -> create a new IAM role (usecase LAMBDA) with s3fullaccess and cloudwatch full access 
+# Create Lambda function and add the role (runtime python) -> add trigger (s3 bucket for POST) -> change code
+# Add image to bucket and check logs
