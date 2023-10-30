35d95a244358607a483ff286a6bcae533bee1405
Meetali Kapse <102135323+meetalik8@users.noreply.github.com>
2023-10-30 06:59:36 +0530

    Update pipelinecloud9.sh

--- advdevops/pipelinecloud9.sh
+++ advdevops/pipelinecloud9.sh
@@ -1,3 +1,13 @@
+git --version
+git config --global user.name "."
+git config --global user.email abc@example.com
+
+aws --version
+git config --global credential.helper '!aws codecommit credential-helper $@'
+git config --global credential.UseHttpPath true
+
+git clone (repository link created in codecommit)
+
 git add <file_name>
 git commit -m "Changes pushed using cloud9"
 git push
